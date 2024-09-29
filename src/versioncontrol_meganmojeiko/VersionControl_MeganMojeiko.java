76d48c0fb6dbe33510086cc5031acaaacfeed7a9
mmojeiko <mmojeiko@dmacc.edu>
2024-09-29 15:35:11 -0500

    Initial commit

---
+++ src/versioncontrol_meganmojeiko/VersionControl_MeganMojeiko.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package versioncontrol_meganmojeiko;
+
+/**
+ *
+ * @author Megan
+ * 
+ */
+public class VersionControl_MeganMojeiko {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is Megan Mojeiko!");
+    }
+}
